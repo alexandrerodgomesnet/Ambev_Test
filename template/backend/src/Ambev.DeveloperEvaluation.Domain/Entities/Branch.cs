e01004c9fa5c5f4a56c77cd476e87c2aa2c67257
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-03-18 18:14:11 -0300

    feat: Create class Branch.

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Branch.cs
@@ -0,0 +1,8 @@
+using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities;
+
+public class Branch : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+}
