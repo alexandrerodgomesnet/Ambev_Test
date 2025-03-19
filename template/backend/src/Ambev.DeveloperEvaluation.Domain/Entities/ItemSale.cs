6c51b2c91fc7bfb6c51155323e7109695277b6c0
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-03-19 10:52:42 -0300

    fix: Adjustment in the ItemSaleValidator class.

--- template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/ItemSale.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/ItemSale.cs
@@ -1,3 +1,6 @@
+using Ambev.DeveloperEvaluation.Common.Validation;
+using Ambev.DeveloperEvaluation.Domain.Validation;
+
 namespace Ambev.DeveloperEvaluation.Domain.Entities;
 
 public class ItemSale
@@ -7,4 +10,15 @@ public class ItemSale
     public decimal UnitPrice { get; set; }
     public decimal Discount { get; private set; }
     public decimal TotalItemValue => UnitPrice * Quantity;
+
+    public ValidationResultDetail Validate()
+    {
+        var validator = new ItemSaleValidator();
+        var result = validator.Validate(this);
+        return new ValidationResultDetail
+        {
+            IsValid = result.IsValid,
+            Errors = result.Errors.Select(o => (ValidationErrorDetail)o)
+        };
+    }
 }
