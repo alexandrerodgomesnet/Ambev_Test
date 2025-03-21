f24742bd2dee316d68487a6b6ace20255f00250a
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-03-21 11:38:56 -0300

    fix: Adjustments class ItemSaleValidator.

--- template/backend/src/Ambev.DeveloperEvaluation.Domain/Validation/ItemSaleValidator.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Validation/ItemSaleValidator.cs
@@ -13,8 +13,10 @@ public class ItemSaleValidator : AbstractValidator<ItemSale>
             .MaximumLength(50).WithMessage("Title sale cannot be longer than 50 characters.");
 
         RuleFor(item => item.Quantity)
-            .NotEqual(0)
-            .WithMessage("Quantity of items cannot be equal to zero.");
+            .GreaterThan(0)
+            .WithMessage("Quantity of items must be greater than zero.")
+            .GreaterThanOrEqualTo(20)            
+            .WithMessage("Quantity of items cannot be greater than 20.");
 
         RuleFor(sale => sale.UnitPrice)
             .NotEqual(0)
