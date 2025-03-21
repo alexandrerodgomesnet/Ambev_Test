ac7a116413dca0606b5000feefcda960ab885a60
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-03-21 11:40:04 -0300

    fix: Adjustments class CreateSaleRequest.

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleRequest.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleRequest.cs
@@ -1,3 +1,4 @@
+using Ambev.DeveloperEvaluation.Common.Validation;
 using Ambev.DeveloperEvaluation.Domain.Entities;
 
 namespace Ambev.DeveloperEvaluation.WebApi.Features.CreateSale.Sales;
@@ -5,7 +6,35 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.CreateSale.Sales;
 public class CreateSaleRequest
 {
     public string Customer { get; set; } = string.Empty;
-    public decimal TotalSaleValue { get; set; }
     public string BranchForSale { get; set; } = string.Empty;
-    public IEnumerable<ItemSale> Products { get; set; } = [];
+    public List<ItemSale> Products { get; set; } = [];
+
+    public ValidationResultDetail Validate()
+    {
+        var validator = new CreateSaleRequestValidator();
+        var result = validator.Validate(this);
+        return new ValidationResultDetail
+        {
+            IsValid = result.IsValid,
+            Errors = result.Errors.Select(o => (ValidationErrorDetail)o)
+        };
+    }
 }
+
+// public sealed record CreateSaleRequest (
+//     string Customer,
+//     string BranchForSale,
+//     IEnumerable<ItemSale> Products
+// )
+// {
+//     public ValidationResultDetail Validate()
+//     {
+//         var validator = new CreateSaleRequestValidator();
+//         var result = validator.Validate(this);
+//         return new ValidationResultDetail
+//         {
+//             IsValid = result.IsValid,
+//             Errors = result.Errors.Select(o => (ValidationErrorDetail)o)
+//         };
+//     }
+// }
