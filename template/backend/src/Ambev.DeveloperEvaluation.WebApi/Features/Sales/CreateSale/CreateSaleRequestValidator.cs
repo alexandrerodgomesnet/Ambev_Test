eb0004bd3a45f6d1ed7e73f3b0b44a08f2178e52
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-03-21 11:40:39 -0300

    fix: Adjustments class CreateSaleRequestValidator.

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleRequestValidator.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleRequestValidator.cs
@@ -1,3 +1,4 @@
+using Ambev.DeveloperEvaluation.Domain.Validation;
 using FluentValidation;
 
 namespace Ambev.DeveloperEvaluation.WebApi.Features.CreateSale.Sales;
@@ -19,9 +20,32 @@ public class CreateSaleRequestValidator : AbstractValidator<CreateSaleRequest>
     /// </remarks>
     public CreateSaleRequestValidator()
     {
-        RuleFor(sale => sale.Customer).NotEmpty().Length(3, 50);
-        RuleFor(sale => sale.TotalSaleValue).NotEqual(0);
-        RuleFor(sale => sale.BranchForSale).NotEmpty().Length(3, 50);
-        RuleFor(sale => sale.Products).NotEmpty();
+        RuleFor(sale => sale.Customer)
+            .MinimumLength(3).WithMessage("Customer must be at least 3 characters long.")
+            .MaximumLength(50).WithMessage("Customer cannot be longer than 50 characters.");
+
+        RuleFor(sale => sale.BranchForSale)
+            .MinimumLength(3).WithMessage("BranchForSale must be at least 3 characters long.")
+            .MaximumLength(50).WithMessage("BranchForSale cannot be longer than 50 characters.");
+
+        RuleForEach(x => x.Products)
+            .ChildRules(itemSale => {
+
+                itemSale.RuleFor(item => item.Title)
+                    .NotEmpty()
+                    .WithMessage("Title sale cannot be empty.")
+                    .MinimumLength(3)
+                    .WithMessage("Title sale must be at least 3 characters long.")
+                    .MaximumLength(50)
+                    .WithMessage("Title sale cannot be longer than 50 characters.");
+
+                itemSale.RuleFor(item => item.Quantity)
+                    .InclusiveBetween(1, 20)
+                    .WithMessage("The quantity must be between 1 and 20 items.");
+
+                itemSale.RuleFor(sale => sale.UnitPrice)
+                    .NotEqual(0)
+                    .WithMessage("Unit price cannot be equal to zero");
+        });
     }
 }
