8a5d9a0e4ba15de5f653198cc3a0df3b2997d260
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-03-24 15:55:55 -0300

    feat: Create features GetSales.

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/GetSale/GetSaleResponse.cs
@@ -0,0 +1,8 @@
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.Response;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale;
+
+/// <summary>
+/// API response model for GetSale operation
+/// </summary>
+public class GetSaleResponse : SaleResponse {}
