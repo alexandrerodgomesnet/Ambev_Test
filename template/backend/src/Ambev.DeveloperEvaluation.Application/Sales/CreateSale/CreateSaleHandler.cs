6b510e6d6d92747f9fa0e6cefcbc7b06a78725fe
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-03-24 15:51:30 -0300

    fix: Adjustment class CreateSale in Application project.

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleHandler.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleHandler.cs
@@ -3,7 +3,6 @@ using FluentValidation;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
 using Ambev.DeveloperEvaluation.Domain.Entities;
 using Ambev.DeveloperEvaluation.Application.Abstractions.Messaging;
-using MediatR;
 
 namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
 
@@ -15,6 +14,7 @@ public class CreateSaleHandler : ICommandHandler<CreateSaleCommand, CreateSaleRe
 {
     private readonly ISaleRepository _saleRepository;
     private readonly IMapper _mapper;
+    private readonly IValidator<CreateSaleCommand> _validator;
 
     /// <summary>
     /// Initializes a new instance of CreateSaleHandler
@@ -22,10 +22,12 @@ public class CreateSaleHandler : ICommandHandler<CreateSaleCommand, CreateSaleRe
     /// <param name="saleRepository">The sale repository</param>
     /// <param name="mapper">The AutoMapper instance</param>
     /// <param name="validator">The validator for CreateSaleCommand</param>
-    public CreateSaleHandler(ISaleRepository saleRepository, IMapper mapper)
+    public CreateSaleHandler(ISaleRepository saleRepository, IMapper mapper,
+        IValidator<CreateSaleCommand> validator)
     {
         _saleRepository = saleRepository;
         _mapper = mapper;
+        _validator = validator;    
     }
 
     /// <summary>
@@ -36,20 +38,12 @@ public class CreateSaleHandler : ICommandHandler<CreateSaleCommand, CreateSaleRe
     /// <returns>The created user details</returns>
     public async Task<CreateSaleResult> Handle(CreateSaleCommand command, CancellationToken cancellationToken)
     {
-        command.MakeDiscount();
-        
-        var validator = new CreateSaleCommandValidator();
-        var validationResult = await validator.ValidateAsync(command, cancellationToken);
+        var validationResult = await _validator.ValidateAsync(command, cancellationToken);
 
         if (!validationResult.IsValid)
             throw new ValidationException(validationResult.Errors);
-
-        // var existingUser = await _saleRepository.GetByEmailAsync(command.Email, cancellationToken);
-        // if (existingUser != null)
-        //     throw new InvalidOperationException($"User with email {command.Email} already exists");
-
+            
         var sale = _mapper.Map<Sale>(command);
-        sale.NotCancelled();
 
         var createdSale = await _saleRepository.CreateAsync(sale, cancellationToken);
         var result = _mapper.Map<CreateSaleResult>(createdSale);
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleProfile.cs
@@ -0,0 +1,37 @@
+using AutoMapper;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Application.Sales.Result;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+
+/// <summary>
+/// Profile for mapping between Sale entity and CreateSaleResult
+/// </summary>
+public class CreateSaleProfile : Profile
+{
+    /// <summary>
+    /// Initializes the mappings for CreateSale operation
+    /// </summary>
+    public CreateSaleProfile()
+    {
+        CreateMap<CreateSaleCommand, Sale>();
+        CreateMap<Sale, CreateSaleResult>()
+            .ConvertUsing(source => new CreateSaleResult()
+                {
+                    Id = source.Id,
+                    Customer = source.Customer,
+                    BranchForSale = source.BranchForSale,
+                    Products = source.Products.Select(p => new ItemSaleResult
+                    {
+                        Id = p.Id,
+                        Title = p.Title,
+                        Quantity = p.Quantity,
+                        UnitPrice = p.UnitPrice,
+                        Discount = p.Discount,
+                        TotalItemValue = p.TotalItemValue
+                    }).ToList(),
+                    Status = source.Status,
+                    TotalSaleValue = source.TotalSaleValue
+                });
+    }
+}
