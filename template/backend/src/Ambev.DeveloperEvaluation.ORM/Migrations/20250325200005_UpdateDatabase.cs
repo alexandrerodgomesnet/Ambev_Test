339ef20e4a947eb128f3fc0e31490a63c93e7aec
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2025-03-26 12:09:51 -0300

    fix: Up migrations.

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Migrations/20250325200005_UpdateDatabase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Sales",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "ItemSales",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ItemSales");
+        }
+    }
+}
